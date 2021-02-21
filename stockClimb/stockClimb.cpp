50fbe4980d2152b476ff79bbd9f358fe9c679a61
johnasharifi <johnasharifi@gmail.com>
2021-02-20 21:34:34 -0800

    Implement getDecisions
    Implement getDecision process.
    
    If we have stock and a downturn is coming, sell.
    If we have cash and an increase is coming, buy

--- stockClimb/stockClimb.cpp
+++ stockClimb/stockClimb.cpp
@@ -23,9 +23,35 @@ std::vector<float> getRandom(int len) {
 	return data;
 }
 
+std::vector<int> getDecisions(const std::vector<float> percentChanges) {
+	// supposing we had perfect market information, and we could decide what to do on each day
+	// decide whether to buy with all our money, sell all our stock, or do nothing each day
+	
+	std::vector<int> decisions;
+	bool hasMoney = true;
+	const float threshold = 1.0f;
+
+	for (float v: percentChanges) {
+		if (hasMoney && v >= threshold) {
+			decisions.push_back(+1);
+			hasMoney = false;
+		}
+		else if (!hasMoney && v <= threshold) {
+			decisions.push_back(-1);
+			hasMoney = true;
+		}
+		else decisions.push_back(0);
+	}
+
+	return decisions;
+}
+
 int main() {
 	std::vector<float> data = getRandom(10);
 	print(data);
+
+	std::vector<int> decisions = getDecisions(data);
+	print(decisions);
 
 	return 0;
 }
