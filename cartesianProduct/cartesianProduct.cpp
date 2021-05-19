b4828dc367918f26e69b1288bf383ea7ec2abac1
johnasharifi <johnasharifi@gmail.com>
2021-05-18 19:20:39 -0700

    Add one generation of cartesian product
    Add manual product generation and writing

--- cartesianProduct/cartesianProduct.cpp
+++ cartesianProduct/cartesianProduct.cpp
@@ -6,8 +6,10 @@ template <typename keyType, typename valueType>
 std::vector<std::unordered_map<keyType, valueType>> cartesianProductWith(std::unordered_map<keyType, valueType> current, const keyType& key, const std::vector<valueType>& values) {
 	std::vector< std::unordered_map<keyType, valueType> > data;
 
+	// TODO do ij iteration through a vector of maps and a range of values here
 	for (int i = 0; i < (int) values.size(); ++i) {
 		data.push_back(current);
+		data[i][key] = values[i];
 	}
 
 	return data;
@@ -28,11 +30,20 @@ std::vector<int> range(int starti, int endi) {
 }
 
 int main() {
+	std::string keys("test");
+	std::vector<int> points = range(0,5);
+
+	// set up our seed generation 
 	std::unordered_map<char, int> map;
 	std::vector< std::unordered_map<char, int> > products;
 	products.push_back(map);
-	products[0]['c'] = 10;
-	products = cartesianProductWith(products[0], 'c', range(0,10));
+
+	// iterate through a cartesian product generations
+	std::vector <std::unordered_map<char, int> > nextProduct = cartesianProductWith(map, 'c', range(0,5));
+	for (int i: range(0,5)) {
+		print(std::string("nextProduct " + std::to_string(i)), nextProduct[i]);
+	}
+
 
 	print(std::string("map"), products[0]);
 }
