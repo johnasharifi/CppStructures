2694c1384ee10dd3059c8a3899a65cb76135264b
johnasharifi <johnasharifi@gmail.com>
2021-06-01 21:57:58 -0700

    Add demo for const ptr
    const ptr can change the address that it points to... but it cannot call an object-mutating method

--- constantReference.cpp
+++ constantReference.cpp
@@ -36,5 +36,19 @@ int main() {
 
 	std::cout << "doubled. value is now " << doubleTest->str11() << "\n";
 
+	const canMultiply* ptrToClass = doubleTest;
+
+	std::cout << "before re-pointing, const ptrToClass address is " << ptrToClass << "\n";
+	// will compile, as the ptr is still a ptr to a constant thing
+	ptrToClass = new canMultiply(3);
+	std::cout << "after re-pointing const ptrToClass to new ptrToClass(3), address is " << ptrToClass << "\n";
+
+	// will not compile, as the const ptr cannot call a potentially-mutating method
+	// ptrToClass->multiply();
+	
+	// can call ptr->str11, as this is a constant method which maintains the const ptr's promise to not mutate
+	std::cout << "ptr value was initialized to 3. value is still " << ptrToClass->str11() << "\n";
+
+	delete ptrToClass;
 	delete doubleTest;
 }
