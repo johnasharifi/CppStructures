36a04e26f60755210fc11eb3035e6db57187653d
johnasharifi <johnasharifi@gmail.com>
2021-06-01 21:48:17 -0700

    Add a const conversion and a commented-out mutator
    Add and comment out a mutator function. The mutator function would not compile

--- constantReference.cpp
+++ constantReference.cpp
@@ -4,24 +4,37 @@
 class canMultiply {
 	private:
 		int i;
+		const int scale = 2;
+
 	public:
 		canMultiply(int _i) {
 			i = _i;
 		}
 
 		void multiply() {
-			
+			i = i * scale;
 		}
-		explicit operator std::string const () {
-			return std::string("");
+
+		// cannot implement a const method which modifies a property of the object
+		/*
+		std::string multiplyAndReport() const {
+			i = i * scale;
+			return std::to_string(i);
+		}
+		*/
+
+		std::string str11 () const {
+			return std::to_string(i);
 		}
 };
 
 int main() {
 	canMultiply* doubleTest = new canMultiply(2);
+	std::cout << "initialized. value is now " << doubleTest->str11() << "\n";
+
 	doubleTest->multiply();
 
-	std::cout << "doubled\n";
+	std::cout << "doubled. value is now " << doubleTest->str11() << "\n";
 
 	delete doubleTest;
 }
